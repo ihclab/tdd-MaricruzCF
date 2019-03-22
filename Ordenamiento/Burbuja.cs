d39fc38bb73196823b5132e6354eab64c39d5752
Maricruz Michel <mmichel5@ucol.mx>
2019-03-22 10:09:23 -0600

    Estrutura de la clase procedimeinto

---
+++ Ordenamiento/Burbuja.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordenamiento {
+	public class Burbuja { 
+		public void Ordenar(int[] arr) { 
+			throw new NotImplementedException(“Crear los casos de prueba");
+		}
+	}
+}
