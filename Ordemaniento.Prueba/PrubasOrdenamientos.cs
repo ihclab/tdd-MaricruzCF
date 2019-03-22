d5aab8c3051a919b46821c2e07ba5b1ce1451707
Maricruz Michel <mmichel5@ucol.mx>
2019-03-22 10:37:43 -0600

    Areglo del proyecto pruebas

---
+++ Ordemaniento.Prueba/PrubasOrdenamientos.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ordemaniento.Prueba
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
