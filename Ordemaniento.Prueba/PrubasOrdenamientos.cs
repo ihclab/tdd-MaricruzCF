7a7fd10fe527c2a368bd77e32d6e61c824a6cfc6
Maricruz Michel <mmichel5@ucol.mx>
2019-03-24 19:22:51 -0600

    pruebas paso #3

--- Ordemaniento.Prueba/PrubasOrdenamientos.cs
+++ Ordemaniento.Prueba/PrubasOrdenamientos.cs
@@ -3,11 +3,14 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Ordemaniento.Prueba
 {
     [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+    public class PruebaOrdenamientos    {
+        private int[] salidaEsperada = new int[]{1, 2, 3, 4, 5};
+        private Ordenamiento.Burbuja ordenar = new Ordenamiento.Burbuja();
+       [TestMethod]
+    public void Ordenado() {
+    int[] entrada = new int[]{1, 2, 3, 4, 5};
+    ordenar.Ordenar(entrada);
+    CollectionAssert.AreEqual(salidaEsperada, entrada);
+}
     }
 }
